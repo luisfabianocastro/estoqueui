cace0c25115ee5bed92fe5e0923568eb073b2bb7
Luis Fabiano <luis-fabianocastro@hotmail.com>
2022-09-08 12:27:14 -0300

    08/09

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', pathMatch:'full', redirectTo: 'produtos'
+  },
+
+  {
+    path:'categorias',
+    loadChildren: ()=> import('./categorias/categorias.module').then(m => m.CategoriasModule)
+  },
+  {
+    path:'produtos',
+    loadChildren: ()=> import('./produtos/produtos.module').then(m=> m.ProdutosModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
